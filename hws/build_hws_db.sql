96b289482e5a351672f4c09327c1f51657ae8776
AJ Pahl <ajpahl1008@gmail.com>
2017-02-12 13:59:49 -0500

    Updated Build Script for HWS

--- hws/build_hws_db.sql
+++ hws/build_hws_db.sql
@@ -1,3 +1,6 @@
-create table HWSDATA (
-   sequence int NOT NULL
-);
+DROP TABLE `HWSDATA`;
+CREATE TABLE `HWSDATA` (
+  `sequence` int(11) NOT NULL,
+  `source` varchar(244) NOT NULL,
+  PRIMARY KEY (`sequence`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
